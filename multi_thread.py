95ffcfff32f959a8b55d19bd4fd15e1bc0e18075
unknown <634754432@qq.com>
2019-08-13 00:39:16 +0800

    update multi_thread.py

--- multi_thread.py
+++ multi_thread.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import json
 import requests
 import time
@@ -5,34 +6,30 @@ import random
 from queue import Queue
 import threading
 import multiprocessing as mp
+import datetime
+import os
+import re
 
 from wenshu_utils.document.parse import parse_detail
 from wenshu_utils.wzws.decrypt import decrypt_wzws
 
-proxy_list = [
-{"https" : "27.40.129.172"},
-{"https" : "210.22.176.146"},
-{"https" : "1.198.72.247"},
-{"https" : "182.34.33.58"},
-{"https" : "211.147.239.101"},
-{"https" : "120.83.99.221"},
-{"https" : "120.83.122.72"},
-{"https" : "221.206.100.133"},
-{"https" : "110.189.152.86"},
-{"https" : "1.192.241.2"},
-{"https" : "113.121.41.96"},
-{"https" : "114.239.149.120"},
-{"https" : "60.10.22.229"},
-{"https" : "163.204.245.179"},
-]
+
+ip_file = open("ip_use_2.0.txt", "r")
+ip_list = []
+for line in ip_file.readlines():
+    ip_list.append({ "https" : line.strip('\n')})
+regex = re.compile(r"[\u4e00-\u9fa5]")
+regex2 = re.compile(r"[0-9]")
+zh_note_list = ['、', '，', '《', '》', '。', '？', '！', '“', '”', '‘', '’', '：', '（', '）', '…', '—', '％']
+
 
 # 代理服务器
 proxyHost = "http-dyn.abuyun.com"
 proxyPort = "9020"
 
 #账号密码
-proxyUser = "H6F573P8H157I04D"
-proxyPass = "EB795750B5FE02BC"
+proxyUser = "H670894FUO7F7F5D"
+proxyPass = "5FDDDFACD81AE0C8"
 proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
       "host" : proxyHost,
       "port" : proxyPort,
@@ -50,9 +47,37 @@ session.headers.update({
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
 })
 
+
 success_time = 0
+total_success_time = 0
+pre_success_time = 0
+delay_time = 0.10
+speed_test_time = 30
+first_time_out = 6
+second_time_out = 4
+total_number_per_file = 100000
+reboot_counter = 0
+max_failing_time = 5
+process_name = "Process-" + str(os.getpid())
+lf_name = "10w_"
+finish = False
+verbose = False
+is_need_reboot = False
+
+
 dump_f = open("test_got_data.txt", 'a', encoding='UTF-8')
-verbose = True
+
+start_from = []
+for i in range(4):
+    try:
+        c = len([ "" for line in open("df" + str(i+1) + ".json", "r", encoding='UTF-8')])
+        start_from.append(c)
+    except:
+        start_from.append(0)
+
+print( process_name + " start from: " + str(start_from) )
+print('\n')
+
 
 class wenshuCrawlThread(threading.Thread):
     # noinspection PyMissingConstructor
@@ -64,8 +89,8 @@ class wenshuCrawlThread(threading.Thread):
         self.size = size
 
     def run(self):
-        global success_time
-        while success_time < self.size:
+        global success_time, is_need_reboot
+        while success_time < self.size and (not is_need_reboot):
             try:
                 json_content = self.jsonQueue.get(False)
             except:
@@ -89,57 +114,296 @@ class wenshuCrawlThread(threading.Thread):
             #finally:
                 #print("%d, %d" %(self.jsonQueue.qsize(),self.dataQueue.qsize()))
 
+
 class parseStoreThread(threading.Thread):
     # noinspection PyMissingConstructor
-    def __init__(self, thread_name, dataQueue, size, lock, file_name):
+    def __init__(self, thread_name, dataQueue, size, lock):
         super(parseStoreThread, self).__init__()
         self.thread_name = thread_name
         self.dataQueue = dataQueue
         self.size = size
         self.lock = lock
-        self.file_name = file_name
 
     def run(self):
-        global success_time
-        while success_time < self.size:
-            time.sleep(0.05)
+        global success_time, total_success_time, is_need_reboot
+        while success_time < self.size and (not is_need_reboot):
+            time.sleep(delay_time)
             try:
                 data = self.dataQueue.get(False)
-                text = get_text(data[0], data[1])
+                j_text = json.dumps( get_JSON_dict(data[0], data[1]), ensure_ascii=False ) + '\n'
+                #text = get_text(data[0], data[1])
                 with self.lock:
-                    dump_f.write(text)
+                    dump_f.write(j_text)
+                    total_success_time += 1
+                    #print(j_text)
                     success_time += 1
                     if verbose:
                         print("Thread " + threading.current_thread().thread_name + " succeed!")
             except:
+                #if verbose:
+                    #print("Thread " + threading.current_thread().thread_name + " failed!")
                 continue
 
 
 def get_data(s):
-    error_msg = "请开启JavaScript并刷新该页"
+    global first_time_out, second_time_out
+    error_msg = "window.location.href="
     url = "http://wenshu.court.gov.cn/CreateContentJS/CreateContentJS.aspx"
     params = {
         "DocID": s,
     }
-    time.sleep(0.05)
-    response = session.get(url, params=params, proxies=random.choice(proxy_list), timeout=8)
+    time.sleep(delay_time)
+    response = session.get(url, params=params, proxies=random.choice(ip_list), timeout=first_time_out)
     text = response.content.decode()
 
     if error_msg in text:
         retry = 3
         for _ in range(retry):
-            redirect_url = decrypt_wzws(text)
-            response = session.get(redirect_url)
+            response = session.get(url, params=params, proxies=random.choice(ip_list), timeout=second_time_out)
             text = response.content.decode()
             if error_msg not in text:
                 break
         else:
-            print("连续{}次获取数据失败".format(retry))
+            if verbose:
+                print("连续{}次获取数据失败".format(retry))
 
     group_dict = parse_detail(response.text)
     #pprint(group_dict)
     return  group_dict
 
+
+def get_zh_text(s):
+    text = ""
+    sl = s.split('>')
+    for ele in sl:
+        if len(re.findall(regex, ele[0])) != 0 or len(re.findall(regex2, ele[0])) != 0 or ele[0] in zh_note_list:
+            second_list = ele.split('<')
+            for ele2 in second_list:
+                if len(re.findall(regex, ele2[0])) != 0 or ele2[0] in zh_note_list:
+                    if len(ele2) < 3:
+                        text = text + ele2
+                    else:
+                        text = text + ele2 + '\n'
+                elif len(re.findall(regex2, ele2[0])) != 0:
+                    if text[len(text)-1] == '\n':
+                        text = text[:len(text)-1]
+                    text = text + ele2
+    return text
+
+
+def get_JSON_dict(group_dict, dict_content):
+    # group_dict 是从网上返回的东西，dict_content是从docid文件里获取的信息
+    j_dict = {}
+    html_content = group_dict['html_data']
+    text = get_zh_text(html_content)
+
+    j_dict["判决书"] = text
+
+    try:
+        head = group_dict['case_info']
+        head = eval(head.replace('null', r'""'))
+        j_dict["诉讼记录段原文"] = head["诉讼记录段原文"]
+        j_dict["案件名称"] = head["案件名称"]
+        j_dict["案号"] = head["案号"]
+
+    except KeyError:
+        if verbose:
+            print("Fatal error: there is no such key in dict!!!\n")
+
+    try:
+        j_dict["案件类型"] = dict_content["案件类型"]
+        j_dict["裁判日期"] = dict_content["裁判日期"]
+        j_dict["案号"] = dict_content["案号"]
+        j_dict["审判程序"] = dict_content["审判程序"]
+    except KeyError:
+        if verbose:
+            print("Fatal error: there is no such key in dict!!!\n")
+
+    return j_dict
+
+
+def timer_task():
+    global pre_success_time, total_success_time, finish, reboot_counter, is_need_reboot, max_failing_time
+
+    # to calculate the speed
+    speed = (total_success_time - pre_success_time) / (speed_test_time/60) * 60
+    pre_success_time = total_success_time
+    print(process_name + ":  " + datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S') + "  speed: " + str(speed) +"/h" + "  total success time: " + str(total_success_time) )
+
+    # to tell if the process need to be reboot
+    if speed == 0:
+        reboot_counter += 1
+        if reboot_counter >= max_failing_time:
+            reboot_counter = 0
+            is_need_reboot = True
+            time.sleep(15)
+            print(process_name + " :连续速度为0已经 " + str(max_failing_time) + " 个" + str(speed_test_time) + "秒, 现在重启进程!!!!!!!!\n")
+            i = int(process_name.replace("Process-", ""))
+            multi_thread_crawler(lf_name + str(i+1) + ".json" ,"df" + str(i+1) + ".json", total_success_time, i)
+            return False
+    else:
+        reboot_counter = 0
+
+    if not finish:
+        t = threading.Timer(speed_test_time, timer_task)
+        t.start()
+
+
+def multi_thread_crawler(load_file_name ,dump_file_name, start, name_number):
+    global success_time, dump_f, count_f, finish, total_number_per_file, is_need_reboot, process_name, reboot_counter, total_success_time
+
+    dump_f.close()
+    process_name = "Process-" + str(name_number)
+    is_need_reboot = False
+    reboot_counter = 0
+    finish = False
+
+    total_success_time = start
+    #print(process_name + " :total success time: " + str(total_success_time) + '\n')
+
+    if total_number_per_file == start:
+        time.sleep(1)
+        finish = True
+        print(process_name + " has already finished!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "  + '\n')
+        return finish
+
+    dump_f = open(dump_file_name, 'a', encoding='UTF-8')
+    load_f = open(load_file_name, 'r', encoding='UTF-8')
+    size = 100
+    writeLock = threading.Lock()
+    thread_number = 25
+
+    # to process the remain data (零头) ==============================================================
+    for _ in range(start):
+        load_f.readline()
+    lingtou = start % size
+    print(process_name + " 零头：" + str(lingtou) )
+    new_start = start
+    if lingtou != 0:
+        remain = size - lingtou
+        new_start = start + remain
+        print(process_name + " 还有 %d 的数据是除去零头的剩余数据，处理完后从第 %d 个数据开始主进程！\n" %(remain, new_start))
+        assert new_start%size == 0
+        js_queue = Queue()
+        da_queue = Queue()
+        success_time = 0
+        for k in range(remain):
+            js_queue.put(load_f.readline())
+
+        #  Speed test -----------------------------------------------------
+        timer = threading.Timer(speed_test_time, timer_task)
+        # 等待speed_test_time间隔调用一次timer_task() 函数
+        timer.start()
+        #  Speed test -----------------------------------------------------
+
+        remain_crawl_thread_list = []
+        for i in range(thread_number):
+            thread = wenshuCrawlThread("Remain_Crawl_thread-" + str(i), js_queue, da_queue, remain)
+            thread.start()
+            remain_crawl_thread_list.append(thread)
+        remain_parse_thread_list = []
+        for i in range(thread_number):
+            thread = parseStoreThread("Rmain_Parse_thread-" + str(i), da_queue, remain, writeLock)
+            thread.start()
+            remain_parse_thread_list.append(thread)
+        for thread in remain_crawl_thread_list:
+            thread.join()
+        for thread in remain_parse_thread_list:
+            thread.join()
+        if is_need_reboot:
+            dump_f.close()
+            return 0
+        print(process_name + ":  " + "Remain %d data processed!!!" %remain )
+    # to process the remain data (零头) ==============================================================
+    else: #就算没有零头也需要启动测速函数
+        #  Speed test -----------------------------------------------------
+        timer = threading.Timer(speed_test_time, timer_task)
+        # 等待speed_test_time间隔调用一次timer_task() 函数
+        timer.start()
+        #  Speed test -----------------------------------------------------
+
+
+    # mian process! ==================================================================================
+    print(process_name + ":  " + "Now start the main process!!!\n")
+    round_times = (total_number_per_file - new_start) // size
+    already_rounds = new_start // size
+    total_round_times = total_number_per_file // size
+
+    for k in range(round_times):
+        j_queue = Queue()
+        d_queue = Queue()
+        success_time = 0
+
+        for i in range(size):
+            j_queue.put(load_f.readline())
+
+        #爬取线程
+        crawl_thread_list = []
+        for i in range(thread_number):
+            thread = wenshuCrawlThread("Crawl_thread-"+str(i), j_queue, d_queue, size)
+            thread.start()
+            crawl_thread_list.append(thread)
+
+        #数据处理、储存线程
+        parse_thread_list = []
+        for i in range(thread_number):
+            thread = parseStoreThread("Parse_thread-"+str(i), d_queue, size, writeLock)
+            thread.start()
+            parse_thread_list.append(thread)
+
+        for thread in crawl_thread_list:
+            thread.join()
+        for thread in parse_thread_list:
+            thread.join()
+        # for rebooting
+        if is_need_reboot:
+            dump_f.close()
+            return 0
+        print(process_name + ":  " + "Number %d/%d round completed!!!\n" %(k+already_rounds+1,total_round_times))
+
+    finish = True
+    load_f.close()
+    dump_f.close()
+    print(process_name + " finished!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    # mian process! ==================================================================================
+
+
+def main():
+    global start_from
+    is_test = False
+
+    if not is_test:
+        is_real_finish = False
+        while not is_real_finish:
+            p = mp.Pool(4)
+            for i in range(4):
+                p.apply_async( multi_thread_crawler, args=(lf_name + str(i+1) + ".json" ,"df" + str(i+1) + ".json", start_from[i], i) )
+            p.close()
+            p.join()
+
+            is_real_finish = True
+            start_from = []
+            for i in range(4):
+                c = len(["" for line in open("df" + str(i + 1) + ".json", "r", encoding='UTF-8')])
+                start_from.append(c)
+
+            for i in range(4):
+                if start_from[i] < total_number_per_file:
+                    is_real_finish = False
+                    print("=========================================================================================\n")
+                    print("The processes finished ABNORMALLY, and now restart the processes!!!!!\n")
+                    print("=========================================================================================\n")
+                    break
+
+        print("=========================================================================================\n")
+        print("The processes have finished normally and now exit!!!!!!!\n")
+        print("=========================================================================================\n")
+
+    else:
+        multi_thread_crawler("10w_1.json", "test.json", 0, 1)
+
+
+'''
 def get_text(group_dict, dict_content):
     text = ""
     html_content = group_dict['html_data']
@@ -165,56 +429,49 @@ def get_text(group_dict, dict_content):
     text = text + case_tpye + judge_date + '\n'
 
     return text
+'''
 
+'''
+def old_get_JSON_dict(group_dict, dict_content):
+    # group_dict 是从网上返回的东西，dict_content是从docid文件里获取的信息
+    text = ""
+    j_dict = {}
+    html_content = group_dict['html_data']
+    arr = html_content.split('FONT-SIZE')
+    arr[0] = arr[0].split('\\')[3][1:]
 
+    for i in range(1, len(arr)):
+        arr[i] = arr[i].split('FONT-FAMILY')[0]
+        arr[i] = arr[i].split('>')[1].rstrip('</div')
 
-def multi_thread_crawl(load_file_name ,dump_file_name):
-    global success_time, dump_f
-    dump_f.close()
-    dump_f = open(dump_file_name, 'a', encoding='UTF-8')
+    for ele in arr:
+        if ele=='<p' or ele=='<title' or ele=='</p' or ele=='':
+            continue
+        text = text + ele + '\n'
 
-    load_f = open(load_file_name, 'r', encoding='UTF-8')
+    j_dict["判决书"] = text
 
-    size = 100
-    thread_number = 8
-    total_success_time = 0
-    writeLock = threading.Lock()
+    try:
+        head = group_dict['case_info']
+        head = eval(head.replace('null', r'""'))
+        j_dict["诉讼记录段原文"] = head["诉讼记录段原文"]
+    except KeyError:
+        if verbose:
+            print("Fatal error: there is no such key[case_info or 诉讼记录原文] in dict!!!\n")
+        j_dict["诉讼记录段原文"] = ""
 
-    for k in range(200):
-        j_queue = Queue()
-        d_queue = Queue()
-        success_time = 0
+    try:
+        j_dict["案件类型"] = dict_content["案件类型"]
+        j_dict["裁判日期"] = dict_content["裁判日期"]
+    except KeyError:
+        if verbose:
+            print("Fatal error: there is no such key[案件类型 or 裁判日期] in dict!!!\n")
+        j_dict["案件类型"] = ""
+        j_dict["裁判日期"] = ""
 
-        for i in range(size):
-            j_queue.put(load_f.readline())
+    return j_dict
+'''
 
-        crawl_thread_list = []
-        for i in range(thread_number):
-            thread = wenshuCrawlThread("Crawl_thread-"+str(i), j_queue, d_queue, size)
-            thread.start()
-            crawl_thread_list.append(thread)
-
-        parse_thread_list = []
-        for i in range(thread_number):
-            thread = parseStoreThread("Parse_thread-"+str(i), d_queue, size, writeLock, "test_got_data.txt")
-            thread.start()
-            parse_thread_list.append(thread)
-
-        for thread in crawl_thread_list:
-            thread.join()
-        for thread in parse_thread_list:
-            thread.join()
-        total_success_time += size
-        print("Number %d round completed!!!\n" %k)
-
-    print(str(total_success_time) + '\n')
-
-def main():
-    p = mp.Pool(4)
-    for i in range(4):
-        p.apply_async(multi_thread_crawl, args=("2w_" + str(i+1) + ".json" ,"df" + str(i+1) + ".txt"))
-    p.close()
-    p.join()
 
 if __name__=='__main__':
     main()
